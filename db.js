6b1fc71f419e5ff18c655c5b87e5f5c12376ff60
tedlopez <tedmlopez@gmail.com>
2021-02-02 17:11:00 -0500

    Adjust settings for production

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
-require('dotenv').config()
-const {Pool} = require("pg");
+require("dotenv").config();
+const { Pool } = require("pg");
+let pool = null;
+let production = process.env.DATABASE_URL;
 
-const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.DATABASE,
-  host: process.env.HOST,
-  port: process.env.PG_PORT,
-});
+if (!production) {
+  pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: "5432",
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+}
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const pool = require("./db.js");
 const playerRouter = require("./routers/playerRouter.js");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/players/", playerRouter);
 
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
